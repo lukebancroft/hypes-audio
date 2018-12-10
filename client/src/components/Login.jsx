a61a40817ec524ac51a1376cfafbddff7149fe25
Fabien LOPEZ <lopez.fab@free.fr>
2018-12-10 18:23:28 +0100

    Login Logout with Facebook Github and Google

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import firestore, { auth, googleProvider, githubProvider, facebookProvider } from "../firestore";
+import {Icon} from 'antd'
+import { auth, googleProvider, githubProvider, facebookProvider } from "../firestore";
 
 export default class Login extends React.Component {
 
@@ -8,7 +9,9 @@ export default class Login extends React.Component {
         this.state = {
             user: null
         }
-        this.login = this.login.bind(this);
+        this.loginWithFacebook = this.loginWithFacebook.bind(this);
+        this.loginWithGithub = this.loginWithGithub.bind(this);
+        this.loginWithGoogle = this.loginWithGoogle.bind(this);
         this.logout = this.logout.bind(this);
     }
 
@@ -23,6 +26,65 @@ export default class Login extends React.Component {
     render() {
         return (
             <div className='app'>
+            <div className="container">
+            {this.state.user ?
+                        <div>
+                            <button onClick={this.logout} className='user-profile btn'>
+                                <img className='user-photo' src={this.state.user.photoURL} />
+                                <div className="logout">LOGOUT</div>
+                            </button>
+                        </div>               
+                        :
+                <form>
+                    <div className="row">
+                        <h2 style={{textAlign:"center"}}>Login with Social Media or Manually</h2>
+                        <div className="vl">
+                            <span className="vl-innertext">or</span>
+                        </div>
+
+                        <div className="col">
+                            <button onClick={this.loginWithFacebook} className="fb btn">
+                                <Icon type="facebook"/> Login with Facebook
+                            </button>
+                            <button onClick={this.loginWithGithub} className="github btn">
+                                <Icon type="github"/> Login with Github
+                            </button>
+                            <button onClick={this.loginWithGoogle} className="google btn">
+                                <Icon type="google"/> Login with Google+
+                            </button>
+                        </div>
+
+                        <div className="col">
+                            <div className="hide-md-lg">
+                                <p>Or sign in manually:</p>
+                            </div>
+
+                            <input type="text" name="username" placeholder="Username" required/>
+                            <input type="password" name="password" placeholder="Password" required/>
+                            <input type="submit" value="Login"/>
+                        </div>
+                    </div>
+                </form>
+            }
+            </div>
+
+            {this.state.user ? <div></div> :
+            <div className="bottom-container">
+                <div className="row">
+                    <div className="col">
+                        <a href="#" style={{color:"white"}} className="btn">Sign up</a>
+                    </div>
+                    <div className="col">
+                        <a href="#" style={{color:"white"}} className="btn">Forgot password?</a>
+                    </div>
+                </div>
+            </div>
+            }
+            </div>
+
+
+
+            /*<div className='app'>
                 <header>
                     <div className="wrapper">
                     <h1>MOD</h1>
@@ -44,7 +106,7 @@ export default class Login extends React.Component {
                     <p>You must be logged in to see the potluck list and submit to it.</p>
                     </div>
                 }
-            </div>
+            </div>*/
         );
     }
 
@@ -57,8 +119,28 @@ export default class Login extends React.Component {
         });
     }
 
-    login() {
+    loginWithGithub() {
+        auth.signInWithPopup(githubProvider) 
+        .then((result) => {
+            const user = result.user;
+            this.setState({
+            user
+            });
+        });
+    }
+
+    loginWithGoogle() {
         auth.signInWithPopup(googleProvider) 
+        .then((result) => {
+            const user = result.user;
+            this.setState({
+            user
+            });
+        });
+    }
+
+    loginWithFacebook() {
+        auth.signInWithPopup(facebookProvider) 
         .then((result) => {
             const user = result.user;
             this.setState({
